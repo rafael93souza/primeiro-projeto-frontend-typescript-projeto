1abc23f6115543b2ecc53598d6933985fa958bd3
Rafael Souza <rafael93.souzasfc5@gmail.com>
2023-07-23 20:37:29 -0300

    :recycle: refactor: update file added routes

--- base/src/index.tsx
+++ base/src/index.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
-import Main from './pages/Main';
+import { MainRouter } from './routes';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <Main />
+    <BrowserRouter>
+        <MainRouter />
+    </BrowserRouter>
   </React.StrictMode>
 );

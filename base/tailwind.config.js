8439c8bdc03921f1f65dde3d5323f95f10b988ff
Rafael Souza <rafael93.souzasfc5@gmail.com>
2023-07-23 20:33:14 -0300

    :sparkles: feat: Configuration tailwind

---
+++ base/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{js,jsx,ts,tsx}"
+      ],
+  theme: {
+    colors: {
+        'primary': '#2E2E36',
+        'second': '#DA0175',
+        'third': '#44444D',
+        'white':'#FFFFFF',  
+      },
+  },
+  plugins: [],
+}
+
